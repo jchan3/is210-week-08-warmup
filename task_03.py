272290da220b1f51563e3752666726f2ae892a15
jchan3 <joechan2014@gmail.com>
2016-10-16 03:37:15 +0000

    task 03

--- task_03.py
+++ task_03.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A complex testcase."""
+"""A complex testcase task_03."""
 
 EXPENSE = 14.23
 LOOKS_NICE = True
 MAX_EXPENSE = 12
 GET_OUT_ALIVE = False
+
+SACRIFICE = ((LOOKS_NICE and EXPENSE <= MAX_EXPENSE) or
+             (not GET_OUT_ALIVE and not(LOOKS_NICE and EXPENSE <= MAX_EXPENSE)))
