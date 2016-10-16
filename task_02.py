494c4983b914c53f268cf42cfd9277213ea4f323
jchan3 <joechan2014@gmail.com>
2016-10-16 03:13:36 +0000

    task 02

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring for task_02."""
+
+
+MY_INTEGER = int(raw_input('What is the answer ' +
+                           'to the life the universe and everything? '))
